281c4966aefd178224e9568d17dea52f2131f704
Erich Brunner <e.brunner@msoftorga.onmicrosoft.com>
2023-07-06 07:45:53 +0200

    added a Behavior

---
+++ TaskyApp/Behaviors/NumericValidationBehavior.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.Forms;
+
+namespace TaskyApp.Behaviors;
+
+public class NumericValidationBehavior : Behavior<Entry>
+{
+    protected override void OnAttachedTo(Entry entry)
+    {
+        entry.TextChanged += EntryOnTextChanged;
+        base.OnAttachedTo(entry);
+    }
+
+    private void EntryOnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        var isValid = double.TryParse(e.NewTextValue, out _);
+        ((Entry)sender).TextColor = isValid ? Color.Green : Color.Red;
+    }
+}
